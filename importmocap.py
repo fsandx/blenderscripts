ae6b88adb772df9eef4e8af6feb94b0a2a2f3713
Fredrik Sandberg <fredsg@axis.com>
2012-11-27 12:08:32 +0100

    cleaning up

--- importmocap.py
+++ importmocap.py
@@ -1,46 +1,60 @@
+# Script for importing Mocap data from Faceshift to Blender,
+# and generate keyframe animation.
+
 import bpy
+import os
 
 context = bpy.context
 scene = context.scene
 object = context.object
 
-fileName = "C:\\Users\\fielduser\\Desktop\\Carolina\\C46_shapekeys.txt"
-fileName2 = "C:\\Users\\fielduser\\Desktop\\Carolina\\C46_shapekeys_1Frame.txt"
-frameNumber = 1
+fileName = "faceshift_mocap.txt"
+path = os.getcwd()
 
-fileObj = open(fileName,"r")
-fileStr = fileObj.read()
-framesData = fileStr.splitlines()
-frames = []
-for frame in framesData:
-	frameList = frame.split(" ")
-	check = 0
-	count = 0
-	values = []
-	for shapeKey in frameList:
-		if check == 1:
-			if shapeKey == "0":
-				shapeKey = 0.0000
-			if shapeKey == "1":
-				shapeKey = 1.0000
-			if shapeKey != "E":
-				values.append(float(shapeKey))
-		if shapeKey == "46":
-			check += 1
-		if shapeKey == "E":
-			check = 0
-		count += 1
-	frames.append(values)
 
-for frameList in frames:
-	for (idx, value) in enumerate(frameList):
-		# get all the id property names of the object that start with index (0 padded 2 digits) underscore.
-		propnames = [name for name in object.keys() if name.startswith("%02d_" % idx)]
-		#should only be one or zero
-		if len(propnames):
-		# ok we have a propname matching the fileindex
-		# set the property that drives the shape to the value
-			object[propnames[0]] = value
-			# insert a keyframe for the value.
-			object.keyframe_insert('["%s"]' % propnames[0], frame=frameNumber)
-	frameNumber += 1
+def getFileData(file):
+    fileObj = open(fileName, "r")
+    fileStr = fileObj.read()
+    framesData = fileStr.splitlines()
+    return framesData
+
+
+def getShapekeyFrames(framesData):
+    frames = []
+    for frame in framesData:
+        frameList = frame.split(" ")
+        check = 0
+        values = []
+        for shapeKey in frameList:
+            if check:
+                if shapeKey != "E":
+                    values.append(float(shapeKey))
+            if shapeKey == "46":
+                check = 1
+            if shapeKey == "E":
+                check = 0
+        frames.append(values)
+    return frames
+
+
+def addKeyFramesAndProperties(shapekeyFrames):
+    frameNumber = 1
+    for frameList in shapekeyFrames:
+        for (idx, value) in enumerate(frameList):
+            # get all the id property names of the object that start with index (0 padded 2 digits) underscore.
+            propnames = [name for name in object.keys() if name.startswith("%02d_" % idx)]
+            #should only be one or zero
+            if len(propnames):
+                # ok we have a propname matching the fileindex
+                # set the property that drives the shape to the value
+                object[propnames[0]] = value
+                # insert a keyframe for the value.
+                object.keyframe_insert('["%s"]' % propnames[0], frame=frameNumber)
+        frameNumber += 1
+
+
+def main():
+    fileData = getFileData(path + fileName)
+    shapekeyFrames = getShapekeyFrames(fileData)
+    addKeyFramesAndProperties(shapekeyFrames)
+
